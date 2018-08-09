ae59d0452def22e1223bc8985a378445c765394e
Jack Palevich <jackpal@google.com>
2018-08-08 21:58:25 -0700

    Use multiple sections.

--- TrashProject/TrashDexViewController.swift
+++ TrashProject/TrashDexViewController.swift
@@ -5,27 +5,30 @@ class TrashDexViewController: PortraitOnlyViewController {
   @IBOutlet weak var collectionView: UICollectionView!
 
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
 
     collectionView.dataSource = self
     collectionView.delegate = self
 
-        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-          flowLayout.itemSize = CGSize(width: collectionView.bounds.width / 2 - 20, height: 120)
-        }
-
+    if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+      flowLayout.itemSize = CGSize(width: collectionView.bounds.width / 2 - 20, height: 120)
     }
-
+  }
 }
 
 extension TrashDexViewController: UICollectionViewDataSource {
+
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return pieceIndex.count
+  }
+
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return allPieces.count
+    return pieceIndex[section].count
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrashCell", for: indexPath) as! TrashDexCollectionViewCell
-    let piece = allPieces[indexPath.row]
+    let piece = pieceIndex[indexPath.section][indexPath.row]
     cell.imageView.image = UIImage(named:piece.name)
     cell.textLabel.text = piece.description
     return cell
